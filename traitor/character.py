decb182f278a5539d5169c85cd576b12f4720707
Daniel Alexander Smith <nalisarc@gmail.com>
2017-02-14 22:58:19 -0700

    Figured out how to use dynamic blocks
    I managed to get the roomlist to pass properly.
    Now all you have to do if you want to make a new room is update
    rooms.org and add it in. Then update the table on index.org by using
    C-c C-c and tangling out.
    Much easier and less room for failure.
    Planning on adding in some additional neatness in the room.org file.

--- traitor/character.py
+++ traitor/character.py
@@ -21,15 +21,10 @@ class Character(object):
         #Inventory
         self.inventory = []
 
-    def change_stat(self, stat, value):
-        """To raise the stat use a positive integer, 
-        to lower the stat use a negative integer"""
-        self.stat['current'] += value
-        #Performs 0 check
-        if self.stat['current'] < 0:
-            self.stat['current'] = 0
-        #Performs 8 check    
-        if self.stat['current'] > 8:
-            self.stat['current'] = 8
-        #Change sucessful
+
+class Stat(object):
+
+    def __init__(self, range_, current):
+        self.range_ = ('dead') + range_
+        self.current = current
         return None
--- traitor/house.py
+++ traitor/house.py
@@ -1,3 +1,4 @@
+room_table=[["Wine Cellar", "(True,False,True,False)", "(-1)"], ["Junk Room", "(True,True,True,True)", "(-1,0,1)"], ["Organ Room", "(False, False, True, True)", "(-1,0,1)"], ["Storeroom", "(True,False,False,False)", "(-1,1)"], ["Creeky Hallway", "(True,True,True,True)", "(-1,0,1)"], ["Dusty Hallway", "(True,True,True,True)", "(-1,0,1)"], ["Furnace Room", "(True,False,True,True)", "(-1)"], ["Stairs from the Basement", "(False,False,True,False)", "(-1)"], ["Operating Laboratory", "(False,True,True,False)", "(-1,1)"], ["Pentagram Chamber", "(False,True,False,False)", "(-1)"], ["Attic", "(False,False,True,False)", "(1)"], ["Chapel", "(True,False,False,False)", "(0,1)"], ["Research Laboratory", "(True,False,True,False)", "(-1,1)"], ["Mystic Elevator", "(True,False,False,False)", "(-1,0,1)"], ["Vault", "(True,False,False,False)", "(-1,1)"], ["Gardens", "(True,False,True,False)", "(0)"], ["Graveyard", "(False,False,True,False)", "(0)"], ["Patio", "(True,False,True,True)", "(0)"], ["Servants' Quarters", "(True,True,True,True)", "(-1,1)"], ["Catacombs", "(True,False,True,False)", "(-1)"], ["Ballroom", "(True,True,True,True)", "(0)"], ["Gymnasium", "(False,True,True,False)", "(-1,1)"], ["Tower", "(False,True,False,True)", "(1)"], ["Larder", "(True,False,True,False)", "(-1)"], ["Bloody Room", "(True,True,True,True)", "(0,1)"], ["Dining Room", "(True,True,False,False)", "(0)"], ["Master Bedroom", "(True,False,False,True)", "(1)"], ["Conservatory", "(True,False,False,False)", "(0,1)"], ["Collapsed Room", "(True,True,True,True)", "(0,1)"], ["Bedroom", "(False,True,False,True)", "(1)"], ["Coal Chute", "(True,False,False,False)", "(1)"], ["Game Room", "(True,True,True,False)", "(-1,0,1)"], ["Library", "(False,False,True,True)", "(0,1)"], ["Charred Room", "(True,True,True,True)", "(0,1)"], ["Abandoned Room", "(True,True,True,True)", "(-1,0)"], ["Balcony", "(True,False,True,False)", "(1)"], ["Statuary Corridor", "(True,False,True,False)", "(-1,0,1)"], ["Underground Lake", "(True,True,False,False)", "(-1)"], ["Kitchen", "(True,True,False,False)", "(-1,0)"], ["Chasm", "(False,True,False,True)", "(-1)"], ["Crypt", "(True,False,False,False)", "(-1)"], ["Gallery", "(True,False,True,False)", "(1)"]]
 import itertools
 import random
 class Room(object):
@@ -131,6 +132,17 @@ class Room(object):
     
     
 
+
+
+
+ROOM_LIST = []
+
+for room in room_table:
+    ROOM_LIST.append(Room(
+        room[0],
+        eval(room[1]),
+        eval(room[2]),)
+    )
 
 
 

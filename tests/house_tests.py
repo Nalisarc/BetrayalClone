9920345bb051febdb1fd492eb0f29b453974a718
Daniel Alexander Smith <nalisarc@gmail.com>
2017-02-26 21:24:35 -0700

    Discover functions
    House ft does not pass at the moment. I'll work on it more tomorrow.

--- tests/house_tests.py
+++ tests/house_tests.py
@@ -3,42 +3,12 @@ import sys
 from traitor import house
 
 
-class HouseFunctionalTests(unittest.TestCase):
 
-    def test_can_not_put_room_on_wrong_floor(self):
+class Discover_Tests(unittest.TestCase):
+    def setUp(self):
+        self.MAP = house.Map()
+        self.ROOM_LIST = house.RoomList(house.room_list)
 
-        testroom = [house.Room(
-            "Evil Room!",
-            (False, False, False, False),
-            (1, 2, 3))]
-        with self.assertRaises(KeyError):
-            house.discover((6,6,6),None,roomlist=testroom)
-    def test_discovered_room_has_coordnate(self):
-        testroom = [house.Room(
-            "Test Room",
-            (True, True, True, True),
-            (-1, 0, 1))]
-        house.discover((-1, 0, 0), None, roomlist=testroom)
-        self.assertNotEqual(house.MAP[(-1, 0, 0)].x, None)
-
-    def test_discovered_room_has_edges(self):
-        testroom = [house.Room(
-        "Test Room",
-            (True, True, True, True),
-            (-1, 0, 1))]
-        house.discover((-2, 0, 0), None, roomlist=testroom)
-        for edge in house.MAP[(-2, 0, 0)].edges:
-            self.assertNotEqual(edge, None)
-
-
-    def test_each_room_is_placeable(self):
-        x = 1
-        y = 0
-        z = 0
-        roomlist = list(house.ROOM_LIST)
-        while roomlist != []:
-            room = roomlist[0]
-            z = room.allowed_floors[0]
-            house.discover((x,y,z),None, roomlist)
-            x += 1
-        self.assertEqual(len(roomlist), 0, "ROOM_LIST did not empty")
+    def test_can_discover_room(self):
+        discover((1,0,0), 'east',MAP=self.MAP,ROOM_LIST=ROOM_LIST)
+        self.MAP.MAP(1,0,0)

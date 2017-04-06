52a495b0ca728210e7226b3a3de653849418ee08
Daniel Alexander Smith <nalisarc@gmail.com>
2017-04-06 11:22:37 -0600

    Deck Tests
    Made modifications to the Deck class, and have tested it for
    representation, indexablility, mutablility, appendablity, and if it is
    can actually be created.
    Things to maybe do, add in a randomizer funtion which will shuffle the
    deck automatically upon creation.  Rewrite RoomLit to use the Deck
    class.

---
+++ tests/test_Deck_object.py
@@ -0,0 +1,54 @@
+import unittest
+
+class Deck(object):
+    def __init__(self, _list):
+        self._list = _list
+        return None
+
+    def __repr__(self):
+        return "Deck({0})".format(self._list)
+
+    def __getitem__(self,position):
+        return self._list[position]
+
+    def __setitem__(self,position,value):
+        self._list[position] = value
+        return None
+
+    def append(self,value):
+        self._list.append(value)
+        return None
+
+
+
+
+class DeckUnitTests(unittest.TestCase):
+
+    def test_can_make_deck(self):
+        test = Deck([])
+        self.assertIsInstance(test,Deck)
+
+    def test_decks_are_indexable(self):
+        test = Deck([1,2,3])
+        self.assertEqual(test[0], 1)
+
+    def test_decks_can_be_appended(self):
+        test = Deck([1,2,3])
+        test.append(4)
+        self.assertEqual(test[3],4)
+
+    def test_decks_can_be_changed(self):
+        test = Deck([1])
+        test[0] = 'a'
+        self.assertEqual(test[0], 'a')
+
+    def test_decks_are_syntactically_symmetrical(self):
+        test1 = Deck([1,2,3])
+        r = repr(test1)
+        test2 = eval(r)
+        self.assertEqual(test1._list,test2._list)
+
+    def test_decks_are_iterable(self):
+        test = Deck([1,2,3])
+        for i in test:
+            self.assertEqual(i, test[i-1])
